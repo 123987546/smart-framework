680ce147bda7aa03daa1e52f7fe566284e249d17
Admin <630843901@qq.com>
2020-04-22 11:37:41 +0800

    最初

---
+++ src/main/java/org/smart4j/framework/util/ClassUtil.java
@@ -0,0 +1,38 @@
+package org.smart4j.framework.util;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+public final class ClassUtil {
+    private static final Logger LOGGER= LoggerFactory.getLogger(ClassUtil.class);
+
+    /**
+     * 获取类加载器
+     * @return
+     */
+    public static ClassLoader getClassLoader(){
+
+    }
+
+    /**
+     * 加载类
+     * @param className
+     * @param isInitialized
+     * @return
+     */
+    public static Class<?> loadClass(String className,boolean isInitialized){
+
+    }
+
+    /**
+     * 获取指定包名下的所有类
+     * @param packageName
+     * @return
+     */
+    public static Set<Class<?>> getClassSet(String packageName){
+
+    }
+}
---
+++ src/main/java/org/smart4j/framework/util/PropsUtil.java
@@ -0,0 +1,16 @@
+package org.smart4j.framework.util;
+
+import java.util.Properties;
+
+public class PropsUtil {
+    public static Properties loadProps(String configFile) {
+    }
+
+    public static String getString(Properties configProps, String jdbcDriver) {
+        return "";
+    }
+
+    public static String getString(Properties configProps, String appJspPath, String s) {
+        return "";
+    }
+}
